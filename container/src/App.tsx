b6948c9c9e7098099aae5e18c2fbfb69c7dec2ef
Sakina D <sa.debiche@gmail.com>
2022-07-01 22:47:08 +0200

    fix

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Container, Toolbar, Button, Typography } from '@mui/material';
-import { StoreProvider, store,useStore } from "store/StoreApp";
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import Header from "../components/HeaderApp";
 import ProductsApp from '../components/ProductsApp';
